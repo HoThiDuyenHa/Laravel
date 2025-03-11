a0b1e1f42e19a8705f25fa8d3fd6ec3ad5235fd7
HoThiDuyenHa <hothiduyenhai2005@gmail.com>
2025-03-11 10:05:04 +0700

    /doneproduct

--- app/Http/Controllers/PageeController.php
+++ app/Http/Controllers/PageeController.php
@@ -10,12 +10,19 @@ use App\Models\Product;
 class PageeController extends Controller
 {
     public function getIndex() {
-        $slide = Slide::all();
-        $products = Product::where('id_type', 6)->limit(4)->get();
-        $product2 = Product::where('id_type', 5)->limit(4)->get();
-        $product3 = Product::where('id_type', 4)->limit(4)->get(); 
+        // Cách 11
+        // $slide = Slide::all();
+        // $products = Product::where('id_type', 6)->limit(4)->get();
+        // $product2 = Product::where('id_type', 5)->limit(4)->get();
+        // $product3 = Product::where('id_type', 4)->limit(4)->get(); 
 
-        return view('page.trangchu', compact('slide', 'products', 'product2', 'product3'));
+        // return view('page.trangchu', compact('slide', 'products', 'product2', 'product3'));
+
+            $slide = Slide::all();
+            $new_product=Product::where('new',1)->paginate(4);
+            $promotion_product=Product::where('promotion_price','<>',0)->paginate(8);
+            return view('page.trangchu', compact('slide', 'new_product', 'promotion_product'));
+
     }
     
     
